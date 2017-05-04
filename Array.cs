46e083e3d2a5c91722a734a6bd9ae47cc7b7e5ba
Steve Lyons <steve@stevelyons.com>
2017-05-03 23:49:26 -0500

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -3,7 +3,7 @@ public class Array
    public static void Main()
    {
      // create an int array 
-     int[] array = new int[11];
+     int[] array = new int[12];
       array[0] = 1;
       array[1] = 2;
       array[2] = 3;
@@ -15,6 +15,7 @@ public class Array
       array[8] = 9;
       array[9] = 10;
       array[10] = 11;
+      array[11] = 12;
       
       Console.WriteLine(array[0]);
       Console.WriteLine(array[1]);
