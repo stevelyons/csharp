00075cd7a441ed1a3f1fda18347296a5153d39e6
Steve Lyons <steve@stevelyons.com>
2017-12-01 22:02:07 -0600

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -5,13 +5,14 @@ public class Addition
       // variables
     int a = 5;
     int b = 3;
-      // mor e vars
+      // more variables
     int c = 7;
     int d = 2;
     int i = 67;
     int e = 9;
     int f = 1;
-      // print the starement 
+    // print the statements
+    // add two variables
     Console.WriteLine(a+b);
     Console.WriteLine(c+d);
     Console.Writeline(a+d);
@@ -19,17 +20,11 @@ public class Addition
     Console.Writeline(a+c);
     Console.Writeline(b+d);
     Console.wtiteline (a+e);
+    // add three variables 
     Console.wtiteline (a+b+c);
     Console.wtiteline (e+g+i);   
-      //add the variables
-      
-      
-      // 
-      Console.wtiteline (a+b+c+e+g+i);
-      Console.wtiteline (i+g+e+c+b+a);
-      
-      
-      Bad code 
-         
+    //add multiple variables
+    Console.wtiteline (a+b+c+e+g+i);
+    Console.wtiteline (i+g+e+c+b+a);    
   }
 }
