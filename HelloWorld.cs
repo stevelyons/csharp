c1860700af5cc35c3d6ff51a06b9bee5ffa48397
Steve Lyons <steve@stevelyons.com>
2017-12-27 23:23:21 -0600

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,9 +3,9 @@ public class HelloWord
    public static void Main()
    {
       // this is a comment 
-      System.Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Hello, World!");
+      // this is another statement
       System.Console.WriteLine("My name is Bob.");
-      // merry christmas
       
    }
 }
