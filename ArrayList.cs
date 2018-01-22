04e6d1a9c7d6a6fbe6d89daefad4f6ae764e41c8
Steve Lyons <steve@stevelyons.com>
2018-01-21 23:25:25 -0600

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -2,6 +2,7 @@ public class Addition
 {
    public static void Main()
    {
+      // create instance of arraylist
       ArrayList myArrayList = new ArrayList();
       myArrayList.Add(56);
       myArrayList.Add("String");
