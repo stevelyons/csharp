f420c191928a00381ae8869735155e5c9fb5f9cd
Steve Lyons <steve@stevelyons.com>
2018-03-22 23:46:20 -0500

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -7,6 +7,7 @@ public class Addition
       ArrayList myArrayList = new ArrayList();
       // add the integer 56 to the ArrayList
       myArrayList.Add(56);
+      myArrayList.AddmyArrayList.Add(12);
       // add string to the ArrayList
       myArrayList.Add("String");
       // create newform
