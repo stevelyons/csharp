ccf5ce9d9182d82660fb13b5db153604cd34ba0c
Steve Lyons <steve@stevelyons.com>
2018-05-18 23:59:56 -0500

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -9,6 +9,7 @@ DateTime dt = new DateTime(2008, 3, 9, 16, 5, 7, 123);
 
 String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
 String.Format("{0:M MM MMM MMMM}", dt);  // "3 03 Mar March"  month
+    thus is dstr 
 String.Format("{0:d dd ddd dddd}", dt);  // "9 09 Sun Sunday" day
 String.Format("{0:h hh H HH}",     dt);  // "4 04 16 16"      hour 12/24
 String.Format("{0:m mm}",          dt);  // "5 05"            minute
