8a248134165d872a6e4562dd6a78f6572cf92cfe
Steve Lyons <steve@stevelyons.com>
2018-06-03 23:27:55 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using System;
 class TestApp
 {
-		// this is the main function 
-		public static void Main(string[] args)
-		{
+   // this is the main function 
+   public static void Main(string[] args)
+{
 			// this is a sample comment 
 			Console.WriteLine("this is the test program");
+	   // second comment
 			Console.WriteLine("this is the test program");
 		}
 	} // end of the program
