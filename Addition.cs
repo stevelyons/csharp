70697722ba6657add534d81f5d2581998fe8de57
Steve Lyons <steve@stevelyons.com>
2018-06-18 23:40:46 -0500

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -3,24 +3,25 @@ public class Addition
 {
    public static void Main()
    {
-      // variables
+      // integers variables
     int a = 5;
     int b = 3;
       // more variables
     int c = 7;
     int d = 2;
+      // more variables
     int i = 67;
     int e = 9;
     int f = 1;
     // print the statements
     // add two variables
-        Console.Writeline(e+f);
-    Console.Writeline(a+c);
-    Console.Writeline(b+d);
     Console.WriteLine(a+b);
     Console.WriteLine(c+d);
     Console.Writeline(a+d);
-  
+    Console.Writeline(e+f);
+    Console.Writeline(a+c);
+      // add variables
+    Console.Writeline(b+d);
     Console.wtiteline (a+e);
     // add three or more variables 
     Console.wtiteline (a+b+c);
@@ -28,6 +29,6 @@ public class Addition
     //add multiple variables
     Console.wtiteline (a+b+c+e+g+i);
     Console.wtiteline (i+g+e+c+b+a);    
-    // the end of the program
+      // end of the program
   }
 }
