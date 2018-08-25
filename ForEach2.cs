eb7c0138e80f0a365f574c92940e852a88877a38
Steve Lyons <steve@stevelyons.com>
2018-08-24 22:26:27 -0700

    Update ForEach2.cs

--- ForEach2.cs
+++ ForEach2.cs
@@ -7,6 +7,7 @@ public class ForEach2
       var names = new List<string>() { "John", "Tom", "Peter" };
       foreach (string name in names)
       {
+         //print ñine 
         Console.WriteLine(name);
       }
   } // end of the program 
