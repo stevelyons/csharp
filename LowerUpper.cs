6bc78159d6cefc1c702b72c0bbe477031a7e0e81
Steve Lyons <steve@stevelyons.com>
2018-10-08 23:32:31 -0500

    Update LowerUpper.cs

--- LowerUpper.cs
+++ LowerUpper.cs
@@ -14,6 +14,7 @@ class LowerUpper
             stringArray.SetValue("Cherry", 4);
     // ser value 
             stringArray.SetValue("WaterMelon", 4);
+    //print the lower bound
             Console.WriteLine("The Lower Bound of the Array : {0}",stringArray.GetLowerBound(0).ToString());
     // upper bounds
             Console.WriteLine("The Upper Bound of the Array : {0}",stringArray.GetUpperBound(0).ToString());
