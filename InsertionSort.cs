0fa8b4798f4d5eb73acce8bf5fc596d0f1913029
Steve Lyons <steve@stevelyons.com>
2018-11-04 22:40:41 -1100

    Update InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -6,7 +6,7 @@ class InsertionSort
         // array
         int[] num = { 69, 56, 77, 44, 8, 0, 111, 566 };
         
-        // for loop
+        // setup the forloop based on the length of the array
         for (int i = 1; i < num.Length; i++)
         {
             // set the key frim the array
