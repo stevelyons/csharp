6a941cd1b6bc771deb930e48f7da3393712e12d2
Steve Lyons <steve@stevelyons.com>
2018-11-07 23:23:00 -0600

    Update BubbleSort.cs

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -4,7 +4,8 @@
          int i, j;
       // length 
          int N = data.Length;
-
+         
+      // for loop to bubble value
          for (j=N-1; j>0; j--) {
             for (i=0; i<j; i++) {
                if (data [i] > data [i + 1])
