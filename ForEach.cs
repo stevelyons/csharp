9da1dc985f108a26add205d9c83601f357a2362e
Steve Lyons <steve@stevelyons.com>
2018-12-09 23:53:59 -0900

    Update ForEach.cs

--- ForEach.cs
+++ ForEach.cs
@@ -5,6 +5,7 @@ class ForEach
     // create instance of array list
     ArrayList list = new ArrayList();
     list.Add("John Doe");
+    // add a new name to array list
     list.Add("Jane Doe");
     //addnae 
     list.Add("Someone Else");
