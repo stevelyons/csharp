e7d17d343aac81acd7612d7bb4c7b201822a825e
Steve Lyons <steve@stevelyons.com>
2018-12-16 23:53:12 -0800

    Update InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -30,6 +30,7 @@ class InsertionSort
         {
             // print the current item
             Console.WriteLine(item);
+            // finish writing
         }
         // display the key
         Console.ReadKey();
