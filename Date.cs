66636821faa6f99f5282c7b16667131b6d8ed3ab
Steve Lyons <steve@stevelyons.com>
2019-01-02 22:56:58 -0600

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -7,6 +7,7 @@ class Date
     // create date time 2008-03-09 16:05:07.123
 DateTime dt = new DateTime(2008, 3, 9, 16, 5, 7, 123);
 
+    // new format
 String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
 String.Format("{0:M MM MMM MMMM}", dt);  // "3 03 Mar March"  month
     thus is dstr 
