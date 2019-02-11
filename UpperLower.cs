79ab7a856f621a2566e4011e7e002e073a0ec9d8
Steve Lyons <steve@stevelyons.com>
2019-02-10 23:45:05 -0600

    Update UpperLower.cs

--- UpperLower.cs
+++ UpperLower.cs
@@ -18,6 +18,7 @@ class UpperLower
             Console.WriteLine("The Lower Bound of the Array : {0}",stringArray.GetLowerBound(0).ToString());
     // upper bound 
             Console.WriteLine("The Upper Bound of the Array : {0}",stringArray.GetUpperBound(0).ToString());
+    // read the last line
             Console.ReadLine();
  
         }
