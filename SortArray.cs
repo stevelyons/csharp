ffc547be738a434017f5ef4af2c81052a27b1afd
Steve Lyons <steve@stevelyons.com>
2019-05-25 23:53:40 -0500

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -8,6 +8,7 @@ public class SortArray
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       // sort the array
       Array.Sort(intArray);
+// prep for 
       // use the foreach method to iterate through the array
       foreach (int i in intArray) 
       {
