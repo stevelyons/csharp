0dfbaee2d57154f09326469ac3e4366f6f025db4
Steve Lyons <steve@stevelyons.com>
2019-06-02 23:35:08 -0500

    Update WhileLoop.cs

--- WhileLoop.cs
+++ WhileLoop.cs
@@ -21,7 +21,7 @@ public class WhileLoop
       {
          // print the current count
          System.Console.WriteLine("The current count is lower than 5.");
-         // increment
+         // increment the current j
          j++;
       }
 // last line 
