0bd03a605eb4e6ec01c63569b6d626bebc2f395a
Steve Lyons <steve@stevelyons.com>
2019-06-04 23:58:25 -0500

    Update ForLoop.cs

--- ForLoop.cs
+++ ForLoop.cs
@@ -12,7 +12,7 @@ class ForLoop
     // decrement by one 
     for(int i = 10; i > 0; i--)
     {
-      // prontrhr number
+      // print the numbers 1-10
       Console.WriteLine("The is a decrement for loop.");
     }
     
