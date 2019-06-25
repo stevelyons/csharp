84fdce9bf7f7468288d82bfc89473d1edb58748a
Steve Lyons <steve@stevelyons.com>
2019-06-24 23:32:18 -0500

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,13 +1,12 @@
+// first program in C#
 Using System;
 public class HelloWord
 {
    // main function
    public static void Main()
    {
-    // print this is a comment 
-System.Console.WriteLine("Hello, World!");
-      // this is another statement
+      // print Hello World
+      System.Console.WriteLine("Hello, World!");
       System.Console.WriteLine("My name is Bob.");
    } // end of the program
-   // end of the main function 
 }
