1752f19620b973cf83d53edb91d6b2ca4e48f4b1
Steve Lyons <steve@stevelyons.com>
2019-06-26 23:34:31 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ class TestApp
    public static void Main(string[] args)
    {
 	  // first comment of the code
+// another comment
 	   Console.WriteLine("this is the test program");
 	   // print the comment "This is the test program"
 	   Console.WriteLine("this is the test program");
