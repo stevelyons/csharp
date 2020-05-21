f79134a34057561060be21197e321c9b84627598
Steve Lyons <steve@stevelyons.com>
2020-05-20 23:28:59 -0700

    Update MayFourth.cs

--- MayFourth.cs
+++ MayFourth.cs
@@ -9,6 +9,7 @@ class MayFourth
         // intitialize the int array with five slots
         int[] arrayA = new int[5];
         // get the length of the stay 
+        int[] arrayB = new int[10];
  
     } // end of the program
 }
