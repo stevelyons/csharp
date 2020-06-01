9235ce1457166e4bb8deed25bcec89835ba9f7d8
Steve Lyons <steve@stevelyons.com>
2020-05-31 23:41:52 -0500

    Update EvenOdd.cs

--- EvenOdd.cs
+++ EvenOdd.cs
@@ -17,8 +17,10 @@ namespace check1
             i = int.Parse(Console.ReadLine());
             if (i % 2 == 0)
             {
-                Console.Write("Entered Number is an Even Number");
-                Console.Read();
+               
+ Console.Write("Entered Number is an Even Number");
+                // read in value
+Console.Read();
             }
             else
             {
