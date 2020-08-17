55c8851dcef43d0c7c8d2529960ea1b0965781fe
Steve Lyons <steve@stevelyons.com>
2020-08-16 23:55:40 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
 // example of writing your first program in C# 
 using System;
-// class
+// main class of the program
 class TestApp
 {
    // main function of the test app
    public static void Main(string[] args)
    {
-
     // print statement to the console
     Console.WriteLine("this is the test program");
-   // print the comment "This is the test program
-
-// print the statement "this is the test program"
+   // print the statement "this is the test program"
    Console.WriteLine("this is the test program");
   }
 } // end of the program
