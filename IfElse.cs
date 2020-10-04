96a20700aabf3d37e8740f62009b16dc31ef5acd
Steve Lyons <steve@stevelyons.com>
2020-10-03 23:58:16 -0500

    Update IfElse.cs

--- IfElse.cs
+++ IfElse.cs
@@ -18,6 +18,7 @@ public class IfElse
       if ((a > c) || (a == b))
         // print the b integer
         Console.WriteLine(b);
+// check it
       // if statement 
       if ((a >= c) && (b <= c))
         // is a is greater and b is smaller than c
