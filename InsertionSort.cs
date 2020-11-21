0d07092e200dd43de79a3d8c5ef15fa2a08c7c01
Steve Lyons <steve@stevelyons.com>
2020-11-20 21:56:22 -0800

    Update InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -7,6 +7,9 @@ class InsertionSort
     {
         // this is the unsorted array [69,56,77,44,8,0,111,566
         int[] num = { 69, 56, 77, 44, 8, 0, 111, 566 };
+        
+        // this is the unsorted array [69,56,77,44,8,0,111,566
+        int[] num2 = { 69, 56, 77, 44, 8, 0, 111, 566 };
         // setup the forloop based on the length of the array
         for (int i = 1; i < num.Length; i++)
         {
@@ -14,8 +17,7 @@ class InsertionSort
             int key = num[i];
             //initialize the j variable and set to the array list minus 1
             int j = i - 1;
-// implement 
-            // while loop to iterate through the loop
+            // while loop to iterate through the key 
             while (j >= 0 && num[j] > key)
             {
                 // while in the loop update the key 
@@ -27,7 +29,7 @@ class InsertionSort
             num[j + 1] = key;
             // end 
         }
-        //display the number from the list 
+        //display
         foreach (var item in num)
         {
             // print the current item
