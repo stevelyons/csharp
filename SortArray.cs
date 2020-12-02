efa1c57e3fba5cec6d09d7f1638c326f431630f3
Steve Lyons <steve@stevelyons.com>
2020-12-01 23:45:58 -0800

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -8,9 +8,11 @@ public class SortArray
       // program to sort an integer array; intialize int array
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       // make a second array 
-      int[] intArray2 = new int[6] { 8, 10, 2, 6, 3, 1 };
+      int[] intArray2 = new int[6] { 6, 9, 2, 6, 5, 10 };
       
-      int[] intArray3 = new int[6] { 18, 10, 15, 6, 8, 1 };
+      int[] intArray3 = new int[6] { 18, 10, 1, 6, 8, 11 };
+      
+      int[] intArray4 = new int[6] { 1, 8, 15, 6, 8, 1 };
       // sort the integer array object intArray
       Array.Sort(intArray); 
       // use the foreach method to iterate through the array object intArray
