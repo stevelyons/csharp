32b71c30e606070aebe64b16d09928b24945424d
Steve Lyons <steve@stevelyons.com>
2021-04-13 22:04:15 -0700

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -4,8 +4,8 @@ class Date
 {
   public static void Main()
     {
-    // create new date object and initialize date 2020-01-09 16:05:07.123
-    DateTime dt = new DateTime(2020, 1, 9, 16, 5, 7, 123);
+    // create new date object dt and initialize date 2021-01-01 16:05:07.123
+    DateTime dt = new DateTime(2021, 1, 1, 16, 5, 7, 123);
 
     // new format of the date 
     String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
@@ -15,11 +15,9 @@ class Date
     String.Format("{0:h hh H HH}",     dt);  // "4 04 16 16"      hour 12/24
     // format the minute 
     String.Format("{0:m mm}",          dt);  // "5 05"            minute
-    String.Format("{0:s ss}",          dt);  // "7 07"     
 
-// new format
-
-       second
+// new format 
+    String.Format("{0:s ss}",          dt);  // "7 07"            second
     String.Format("{0:f ff fff ffff}", dt);  // "1 12 123 1230"   sec.fraction
     String.Format("{0:F FF FFF FFFF}", dt);  // "1 12 123 123"    without zeroes
     String.Format("{0:t tt}",          dt);  // "P PM"            A.M. or P.M.
