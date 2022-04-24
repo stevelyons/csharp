8bc3ffe23e8792200568c6a24b1758aea88606a3
Steve Lyons <steve@stevelyons.com>
2022-04-24 02:00:12 -0500

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -118,9 +118,11 @@ public class Array
       Console.WriteLine(arrray[27]);
       // print the twenty-ninth element
       Console.WriteLine(arrray[28]);
-      // print the thirtieth element
+      // print the thirth element
       Console.WriteLine(arrray[29]);
+      // print the c
       Console.WriteLine(array[30]);
+      // print the   thirth element
       Console.WriteLine(array[31]);
       Console.Writelibe(array[32]);
       Console.WriteLine(array[33]);
