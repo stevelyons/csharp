349ff71d37bc8e4ab4a985ac14634361f93dc682
Steve Lyons <steve@stevelyons.com>
2023-01-20 20:18:20 -0600

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -386,6 +386,12 @@ public class Array
       
       // set the value of the index 
       array[178] = 166;
+      
+      
+      
+          
+      // set the value of the index 
+      array[178] = 166;
 
       // print the first element 
       Console.WriteLine(array[0]);
