095079be6942a5d8a6614c22a34c8bff6ee399eb
Steve Lyons <steve@stevelyons.com>
2024-02-17 23:14:48 -0600

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -28,9 +28,7 @@ public class Array
       array[17] = 17;
       array[18] = 18;
       array[19] = 19;
-   
       array[20] = 20;
-      // set the value of the index 
       array[21] = 21;
        // set the value of the index
       array[22] = 22;
