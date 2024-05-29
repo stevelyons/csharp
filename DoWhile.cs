2c3302d12e3e656b14cb7db2094a08b0e764daae
Steve Lyons <steve@stevelyons.com>
2024-05-28 16:48:56 -1100

    Update DoWhile.cs

--- DoWhile.cs
+++ DoWhile.cs
@@ -8,14 +8,14 @@ public class DoWhile
     // set the count to 3
     int x = 8;
     int y = 10;
-int d = 6;
+    int d = 6;
       // execute the action at least once 
       do 
       {
          // add the increment 
         // increment the variable x by 1 
         x++;
-// new x count 
+        // new x count 
       }
       // while the x variable is less than 10 
      while(x < 10)
