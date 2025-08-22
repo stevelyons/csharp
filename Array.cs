13af7cea7d8794b36aa2d259578a364110b0f65c
Steve Lyons <steve@stevelyons.com>
2025-08-21 15:47:40 -1100

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -6,7 +6,6 @@ public class Array
    {
       // create an int array of 60 indexes 
       int[] array = new int[60];
-      array[0[ = 0;
       array[0] = 0;
       array[1] = 1;
       array[2] = 2;
