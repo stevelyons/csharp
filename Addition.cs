e7a6bf216585101efd4ddf7b45a8f964cfd3afe8
Steve Lyons <steve@stevelyons.com>
2026-09-16 16:39:53 -0800

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -4,7 +4,7 @@ public class Addition
    public static void Main()
    {
       // initialize int variables
-      int x = 4;
+      int x = 40;
       int y = 10;
       int a = 8;
       int z = x;
